a8e3ccd65a9871b3a9465e928a8661a56fe11e47
John <john@trabian.com>
2020-09-30 00:30:02 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import sampleWeather from './sampleWeather.json'
+import { WeatherLocale, WeatherData, WeatherError } from './types'
 
-function App() {
+const apiKey = 'a889ab39f32412f8e65caec1d77a1baa'
+const localeIn: WeatherLocale = {
+  name: 'Indiana',
+  latitude: 39.96,
+  longitude: -86.01,
+}
+const localeTn: WeatherLocale = {
+  name: 'Tennessee',
+  latitude: 35.98,
+  longitude: -87.12,
+}
+
+const WeatherDisplay = ({ weather }: { weather: WeatherData }) => {
+  const [currentWeatherIcon, setCurrentWeatherIcon] = useState<
+    string | undefined
+  >(undefined)
+
+  const iconCode = weather.current.weather[0].icon
+
+  const iconURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+  const getIcon = async (iconURL: string) =>
+    await fetch(iconURL, { method: 'GET' }).then((response) =>
+      response.json().then((data: string) => setCurrentWeatherIcon(data)),
+    )
+
+  getIcon(iconURL)
+
+  return <div>{JSON.stringify(currentWeatherIcon)}</div>
+}
+
+const WeatherErrorDisplay = ({ error }: { error: WeatherError }) => {
+  return <div>{JSON.stringify(error)}</div>
+}
+
+const App = () => {
+  const [weather, setWeather] = useState<WeatherData | undefined>(sampleWeather)
+
+  const [error, setError] = useState<WeatherError | undefined>(undefined)
+
+  const [locale, setLocale] = useState<WeatherLocale>(localeIn)
+
+  const requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${locale.latitude}&lon=${locale.longitude}&appid=${apiKey}`
+
+  const getWeather = async (requestURL: string) =>
+    await fetch(requestURL, {
+      method: 'GET',
+    }).then((response) => {
+      response.json().then((value: WeatherData | WeatherError) => {
+        if ('cod' in value) {
+          setError(value)
+        } else {
+          setWeather(value)
+        }
+      })
+    })
+
+  const changeLocale = () => {
+    if (locale.name == 'Indiana') {
+      setLocale(localeTn)
+    } else {
+      setLocale(localeIn)
+    }
+  }
+
+  // useEffect(() => {
+  //   getWeather(requestURL)
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {weather ? <WeatherDisplay weather={weather} /> : 'No Weather Today'}
         </p>
+        <p>{error ? <WeatherErrorDisplay error={error} /> : null}</p>
+        <p>{locale.name}</p>
+        <button onClick={changeLocale}>change locale</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +92,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/types.ts
@@ -0,0 +1,93 @@
+type Forecast = {
+  id: number
+  main: string
+  description: string
+  icon: string
+}
+
+type CurrentWeather = {
+  dt: number
+  sunrise: number
+  sunset: number
+  temp: number
+  feels_like: number
+  pressure: number
+  humidity: number
+  dew_point: number
+  uvi: number
+  clouds: number
+  visibility: number
+  wind_speed: number
+  wind_deg: number
+  weather: Forecast[]
+}
+
+type MinutelyForecast = { dt: number; precipitation: number }
+
+type HourlyForecast = {
+  dt: number
+  temp: number
+  feels_like: number
+  pressure: number
+  humidity: number
+  dew_point: number
+  clouds: number
+  visibility: number
+  wind_speed: number
+  wind_deg: number
+  weather: Forecast[]
+  pop: number
+}
+
+type Temp = {
+  day: number
+  min: number
+  max: number
+  night: number
+  eve: number
+  morn: number
+}
+
+type FeelsLike = {
+  day: number
+  night: number
+  eve: number
+  morn: number
+}
+
+type DailyForecast = {
+  dt: number
+  sunrise: number
+  sunset: number
+  temp: Temp
+  feels_like: FeelsLike
+  pressure: number
+  humidity: number
+  dew_point: number
+  wind_speed: number
+  wind_deg: number
+  weather: Forecast[]
+  clouds: number
+  pop: number
+  rain?: number
+  uvi: number
+}
+
+export type WeatherData = {
+  lat: number
+  lon: number
+  timezone: string
+  timezone_offset: number
+  current: CurrentWeather
+  minutely: MinutelyForecast[]
+  hourly: HourlyForecast[]
+  daily: DailyForecast[]
+}
+
+export type WeatherError = { cod: number; message: string }
+
+export type WeatherLocale = {
+  name: string
+  latitude: number
+  longitude: number
+}
